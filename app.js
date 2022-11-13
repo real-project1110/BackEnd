23f72db607c724426d6d191d025402aa58a44b8b
jeonghyeon <kjhhy1005@gmail.com>
2022-11-14 02:46:21 +0900

    feat: errorLogger, errorHandler 적용

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const port = 4000;
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const {
+  errorLoger,
+  errorHandler,
+} = require('./middlewares/error-handler.middleware');
 const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', routes);
+app.use(errorLoger);
+app.use(errorHandler);
 app.listen(port, () => {
   console.log('Hi server open :', port);
 });
