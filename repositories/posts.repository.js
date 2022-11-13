e21b23c1dd06a942f069dc210677fc82c3081fdc
jeonghyeon <kjhhy1005@gmail.com>
2022-11-14 05:28:37 +0900

    feat : groupUserId 찾기

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,4 +1,4 @@
-const { Post } = require('../models');
+const { Post, GroupUser } = require('../models');
 
 class PostRepository extends Post {
   constructor() {
@@ -7,6 +7,11 @@ class PostRepository extends Post {
   //*게시글 작성
   createPost = async ({ post }) => {
     const createPost = await Post.create({ ...post });
+    return createPost;
+  };
+  findGroupUserId = async ({ userId }) => {
+    const findGroupUserId = await GroupUser.findByPk(userId);
+    return findGroupUserId.groupUserId;
   };
 }
 module.exports = PostRepository;
