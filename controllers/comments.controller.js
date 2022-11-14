872d3fba74e6b24fc4bd16f1641e23ba59851eb8
jeonghyeon <kjhhy1005@gmail.com>
2022-11-14 23:58:51 +0900

    feat : 댓글 작성 controller #3

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,5 +3,27 @@ const InvalidParamsError = require('../exceptions/index.exception');
 
 class CommentController {
   commentService = new CommentService();
+
+  //*댓글 작성
+  createComment = async (req, res, next) => {
+    try {
+      const { postId } = req.params;
+      const { userId } = res.locals.user;
+      const { comment } = req.body;
+      if (!postId || !userId) {
+        throw new InvalidParamsError('잘못된 요청입니다.');
+      }
+      const createComment = await this.commentService.createComment({
+        postId,
+        userId,
+        comment,
+      });
+      res.status(201).json({
+        ok: true,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 module.exports = CommentController;
