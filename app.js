2e88796003c66eab8e8be375ff474efa1d97bbca
jeonghyeon <kjhhy1005@gmail.com>
2022-11-15 04:37:46 +0900

    feat : morgan 추가

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 4000;
 const cookieParser = require('cookie-parser');
@@ -10,6 +11,7 @@ const {
 const routes = require('./routes');
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
