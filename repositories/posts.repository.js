0fc1c29977ea01a6844b86c4e0dc04f5dd1da383
jeonghyeon <kjhhy1005@gmail.com>
2022-11-14 22:39:16 +0900

    feat: 게시글 상세조회 repository

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,5 +1,7 @@
 const { Post, GroupUser } = require('../models');
 const { Op } = require('sequelize');
+const Sq = require('sequelize');
+const Sequelize = Sq.Sequelize;
 
 class PostRepository extends Post {
   constructor() {
@@ -10,10 +12,12 @@ class PostRepository extends Post {
     const createPost = await Post.create({ ...post });
     return createPost;
   };
+  //*그룹유저 찾기
   findGroupUserId = async ({ userId }) => {
     const findGroupUserId = await GroupUser.findByPk(userId);
     return findGroupUserId;
   };
+  //*게시글 전체 조회
   findAllPost = async ({ groupId, category }) => {
     const findAllPost = await Post.findAll({
       where: { [Op.and]: [{ groupId }, { category }] },
@@ -29,6 +33,25 @@ class PostRepository extends Post {
       order: ['createdAt', 'DESC'],
     });
     return findAllPost;
+  };
+  //*게시글 상세 조회
+  //postImg 추가해야함
+  findPost = async ({ postId }) => {
+    const findPost = await Post.findOne({
+      where: { postId },
+      attributes: [
+        postId,
+        title,
+        content,
+        commentCount,
+        createdAt,
+        [Sequelize.col('GroupUser.groupUserId'), 'groupUserId'],
+        [Sequelize.col('GroupUser.groupUserNickname'), 'groupUserNickname'],
+        // [Sequelize.col('GroupUser.groupAvatarimg'), 'groupAvatarimg'],
+      ],
+      include: [{ model: GroupUser }],
+    });
+    return findPost;
   };
 }
 module.exports = PostRepository;
