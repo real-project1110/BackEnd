22dcef503f2fec75a0511af6f214bd0a32c00e19
jeonghyeon <kjhhy1005@gmail.com>
2022-11-14 23:59:12 +0900

    feat : 댓글 작성 service #3

--- services/comments.service.js
+++ services/comments.service.js
@@ -1,7 +1,25 @@
 const CommentRepository = require('../repositories/comments.repository');
+const PostRepository = require('../repositories/posts.repository');
 const ValidationError = require('../exceptions/index.exception');
 
 class CommentService {
   commentRepository = new CommentRepository();
+  postRepository = new PostRepository();
+
+  //*댓글 작성
+  createComment = async ({ postId, userId, comment }) => {
+    const findGroupUserId = await this.postRepository.findGroupUserId({
+      userId,
+    });
+    if (!findGroupUserId) {
+      throw new ValidationError('잘못된 요청입니다.');
+    }
+    const createComment = await this.commentRepository.createComment({
+      postId,
+      comment,
+      groupUserId: findGroupUserId.groupUserId,
+    });
+    return createComment;
+  };
 }
 module.exports = CommentService;
