1d06dd2500f1da875ce100c198212b7836d528a7
jeonghyeon <kjhhy1005@gmail.com>
2022-11-15 00:01:14 +0900

    feat : 댓글 작성 repository #3

--- repositories/comments.repository.js
+++ repositories/comments.repository.js
@@ -7,5 +7,14 @@ class CommentRepository extends Comment {
   constructor() {
     super();
   }
+  //*댓글 작성
+  createComment = async ({ postId, comment, groupUserId }) => {
+    const createComment = await Comment.create({
+      postId,
+      comment,
+      groupUserId,
+    });
+    return createComment;
+  };
 }
 module.exports = CommentRepository;
