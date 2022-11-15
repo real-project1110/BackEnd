eed619f55f8614b96183286a16521532c6642d9e
jeonghyeon <kjhhy1005@gmail.com>
2022-11-15 15:45:06 +0900

    feat : search repository 뼈대 #16

---
+++ repositories/searches.repository.js
@@ -0,0 +1,9 @@
+const { User, Group, Comment, Post, GroupUser } = require('../models');
+const { Op } = require('sequelize');
+
+class SearchRepository {
+  constructor() {
+    super();
+  }
+}
+module.exports = SearchRepository;
