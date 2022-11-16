40a051ed411f7ad14babbbdf837e07cbc405ed61
donghee44 <113874655+donghee44@users.noreply.github.com>
2022-11-16 17:57:30 +0900

    feat.color,schedule db 생성

---
+++ migrations/8-create-schedule.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Schedules', {
+      scheduleId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      start: {
+        type: Sequelize.STRING
+      },
+      end: {
+        type: Sequelize.STRING
+      },
+      colorId: {
+        type: Sequelize.INTEGER
+      },
+      groupUserId: {
+        type: Sequelize.INTEGER
+      },
+      groupId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Schedules');
+  }
+};
