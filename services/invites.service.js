991803740849ae437fbca588ac3fa819265a2f41
jeonghyeon <kjhhy1005@gmail.com>
2022-11-23 05:17:41 +0900

    fix : return 수정

--- services/invites.service.js
+++ services/invites.service.js
@@ -1,17 +1,16 @@
-const InviteRepository = require('../repositories/invites.repository')
+const InviteRepository = require('../repositories/invites.repository');
 
 class InviteService {
-    inviteRepository = new InviteRepository()
+  inviteRepository = new InviteRepository();
 
-    createInvite = async(email,groupId)=>{
-        await this.inviteRepository.createInvite(email,groupId)
-        return {message:"초대 성공"}
-    }
+  createInvite = async (email, groupId) => {
+    await this.inviteRepository.createInvite(email, groupId);
+  };
 
-    findInvite = async(userId)=>{
-        const invite = await this.inviteRepository.findInvite(userId)
-        return invite
-    }
+  findInvite = async (userId) => {
+    const invite = await this.inviteRepository.findInvite(userId);
+    return invite;
+  };
 }
 
-module.exports = InviteService
+module.exports = InviteService;
