4e851adea30e3bc87fef0d0e20a0c22a49e4972c
jeonghyeon <kjhhy1005@gmail.com>
2022-11-25 22:14:40 +0900

    fix : 이미지 개수 설정

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -10,7 +10,7 @@ const postController = new PostController();
 router.post(
   '/:groupId/posts',
   auth,
-  upload.array('image', 5),
+  upload.array('image'),
   postController.createPost,
 );
 //*게시글 전체 조회
