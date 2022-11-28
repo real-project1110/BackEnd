63e859a1919bb8973d55e9627282ea24645d0895
jeonghyeon <kjhhy1005@gmail.com>
2022-11-28 23:03:36 +0900

    fix : groupId 추가 수정

--- repositories/colors.repository.js
+++ repositories/colors.repository.js
@@ -1,52 +1,52 @@
-const {Color,GroupUser} = require('../models');
-const {Op} = require('sequelize')
-class ColorRepository{
-    createColor = async({groupId,color,content}) =>{
-        console.log(color)
-        const createColor = await Color.create({groupId,color,content})
-        console.log('1111',createColor)
-        return createColor
-    }
+const { Color, GroupUser } = require('../models');
+const { Op } = require('sequelize');
+class ColorRepository {
+  createColor = async ({ groupId, color, content }) => {
+    console.log(color);
+    const createColor = await Color.create({ groupId, color, content });
+    console.log('1111', createColor);
+    return createColor;
+  };
 
-    // findGU = async(userId,groupId) =>{
-    //     const findGU = await GroupUser.findOne({
-    //         where: { [Op.and]: [{ userId }, { groupId }] }
-    //     })
-    //     console.log('222222222',findGU)
-    //     return findGU
-    // }
+  // findGU = async(userId,groupId) =>{
+  //     const findGU = await GroupUser.findOne({
+  //         where: { [Op.and]: [{ userId }, { groupId }] }
+  //     })
+  //     console.log('222222222',findGU)
+  //     return findGU
+  // }
 
-    getGroupId = async({userId})=>{
-        const getGroupId = await GroupUser.findOne({where:{userId}})
-        return getGroupId
-    }
+  getGroupId = async ({ userId, groupId }) => {
+    const getGroupId = await GroupUser.findOne({ where: { groupId, userId } });
+    return getGroupId;
+  };
 
-    findGroupId= async(groupId)=>{
-        const findGroupId = await Color.findAll({
-            where : {groupId},
-            order : [['colorId','desc']],
-        })
-        return findGroupId 
-    }
+  findGroupId = async (groupId) => {
+    const findGroupId = await Color.findAll({
+      where: { groupId },
+      order: [['colorId', 'desc']],
+    });
+    return findGroupId;
+  };
 
-    // findUC = async(userId,colorId)=>{
-    //     const findUC = await Color.findOne({
-    //         where: { [Op.and]: [{ userId }, { colorId }, {groupId}] }
-    //     })
-    //     return findUC
-    // }
+  // findUC = async(userId,colorId)=>{
+  //     const findUC = await Color.findOne({
+  //         where: { [Op.and]: [{ userId }, { colorId }, {groupId}] }
+  //     })
+  //     return findUC
+  // }
 
-    updateColor = async(colorId,groupId,color,content)=>{
-        const updateColor = await Color.update(
-            {color,content},
-            {where: {colorId,groupId}}
-        )
-        return updateColor
-    }
-    
-    deleteColor = async(colorId)=>{
-        const deleteColor = await Color.destroy({where: {colorId}})
-        return deleteColor
-    }
+  updateColor = async (colorId, groupId, color, content) => {
+    const updateColor = await Color.update(
+      { color, content },
+      { where: { colorId, groupId } },
+    );
+    return updateColor;
+  };
+
+  deleteColor = async (colorId) => {
+    const deleteColor = await Color.destroy({ where: { colorId } });
+    return deleteColor;
+  };
 }
-module.exports = ColorRepository
+module.exports = ColorRepository;
--- services/colors.service.js
+++ services/colors.service.js
@@ -1,75 +1,86 @@
 const { CloudFormation } = require('aws-sdk');
-const ColorRepository = require('../repositories/colors.repository')
+const ColorRepository = require('../repositories/colors.repository');
 
 class ColorService {
-    colorRepository = new ColorRepository();
+  colorRepository = new ColorRepository();
 
-    createColor = async({userId,groupId,color,content})=>{
-        const getGroupId = await this.colorRepository.getGroupId({userId})
-        // if(userId !==getGroupId.userId){
-        //     throw new Error('유저 정보가 없습니다')
-        // }
-        if(!getGroupId){
-            throw new Error('유저 정보가 없습니다')
-        }
-        if(getGroupId.groupId !==parseInt(groupId)){
-            throw new Error('소속된 그룹이 아닙니다')
-        }
-        // console.log('11111111',groupId,getGroupId.groupId)
-        // console.log('123123123123',userId)
-        console.log(color)
-        const createColor = await this.colorRepository.createColor({groupId,color,content})
-        if(createColor.groupId !==groupId){
-            throw new Error('권한이 없습니다')
-        }
-        console.log('111111',createColor.groupId,groupId)
-        return {
-            groupId: getGroupId.groupId,
-            color : createColor.color,
-            content : createColor.content
-        }
+  createColor = async ({ userId, groupId, color, content }) => {
+    const getGroupId = await this.colorRepository.getGroupId({
+      groupId,
+      userId,
+    });
+    // if(userId !==getGroupId.userId){
+    //     throw new Error('유저 정보가 없습니다')
+    // }
+    if (!getGroupId) {
+      throw new Error('유저 정보가 없습니다');
     }
-    getColor = async(groupId)=>{
-        const getColor = await this.colorRepository.findGroupId(groupId)
-        if(!getColor){
-            throw new Error('유저 정보가 존재하지 않습니다')
-        }
-        const result = getColor.map((x)=>{
-            return {
-                colorId : x.colorId,
-                groupId : x.groupId,
-                color : x.color,
-                content : x.content
-            }
-        })
-        return result
+    if (getGroupId.groupId !== parseInt(groupId)) {
+      throw new Error('소속된 그룹이 아닙니다');
+    }
+    // console.log('11111111',groupId,getGroupId.groupId)
+    // console.log('123123123123',userId)
+    console.log(color);
+    const createColor = await this.colorRepository.createColor({
+      groupId,
+      color,
+      content,
+    });
+    if (createColor.groupId !== groupId) {
+      throw new Error('권한이 없습니다');
+    }
+    console.log('111111', createColor.groupId, groupId);
+    return {
+      groupId: getGroupId.groupId,
+      color: createColor.color,
+      content: createColor.content,
+    };
+  };
+  getColor = async (groupId) => {
+    const getColor = await this.colorRepository.findGroupId(groupId);
+    if (!getColor) {
+      throw new Error('유저 정보가 존재하지 않습니다');
+    }
+    const result = getColor.map((x) => {
+      return {
+        colorId: x.colorId,
+        groupId: x.groupId,
+        color: x.color,
+        content: x.content,
+      };
+    });
+    return result;
+  };
+
+  updateColor = async (userId, groupId, colorId, color, content) => {
+    const getGroupId = await this.colorRepository.getGroupId(userId);
+    if (!getGroupId) {
+      throw new Error('유저 정보가 없습니다');
+    }
+    const updateColor = await this.colorRepository.updateColor(
+      groupId,
+      colorId,
+      color,
+      content,
+    );
+    return {
+      color: updateColor.color,
+      content: updateColor.content,
+    };
+  };
+
+  deleteColor = async (userId, colorId) => {
+    const getGroupId = await this.colorRepository.getGroupId(userId);
+    if (!getGroupId) {
+      throw new Error('유저 정보가 없습니다');
     }
 
-    updateColor = async(userId,groupId,colorId,color,content)=>{
-        const getGroupId = await this.colorRepository.getGroupId(userId)
-        if(!getGroupId){
-            throw new Error('유저 정보가 없습니다')
-        }
-        const updateColor = await this.colorRepository.updateColor(groupId,colorId,color,content)
-        return{
-            color :updateColor.color,
-            content : updateColor.content
-        }
+    const deleteColor = await this.colorRepository.deleteColor(colorId);
+    if (!deleteColor) {
+      throw new Error('존재하지 않는 컬러입니다');
     }
-
-    deleteColor = async(userId,colorId)=>{
-        const getGroupId = await this.colorRepository.getGroupId(userId)
-        if(!getGroupId){
-            throw new Error('유저 정보가 없습니다')
-        }
-        
-        const deleteColor = await this.colorRepository.deleteColor(colorId)
-        if(!deleteColor){
-            throw new Error('존재하지 않는 컬러입니다')
-        }
-        return deleteColor
-    }
+    return deleteColor;
+  };
 }
-
 
 module.exports = ColorService;
