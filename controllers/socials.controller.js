9f5876898e562e6c5ec4013c96ae5df2d688e4e8
hyunjin9603 <hyunjin9603@gmail.com>
2022-12-02 00:43:06 +0900

    feat:소셜카카오front랑연동완료

---
+++ controllers/socials.controller.js
@@ -0,0 +1,11 @@
+// const SocialService = require('../services/socials.service');
+
+// class SocialController {
+//     socialService = new SocialService();
+    
+//     kakaosignup = async(req,res,next)=>{
+//         const createUser = await this.socialService.createUser({email,nickname,avatarImg,provider})
+//         res.status(201).json({data:createUser})
+//     }
+// }
+// module.exports = SocialController;
