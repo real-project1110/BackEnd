7dd63752205bcbdc8c36a7c4d75263e7f3b92359
jeonghyeon <kjhhy1005@gmail.com>
2022-12-03 08:36:01 +0900

    feat : #107

--- socket.js
+++ socket.js
@@ -1,5 +1,6 @@
 const socketIo = require('socket.io');
 
+const onlineMap = {};
 module.exports = (server) => {
   const io = socketIo(server, { path: '/socket.io' });
   //   const socketIdMap = {};
@@ -49,33 +50,72 @@ module.exports = (server) => {
   //     };
   //   }
   // };
-  io.on('connection', (socket) => {
-    const req = socket.request;
-    console.log('연결완료');
-
-    socket.on('disconnect', () => {
-      clearInterval(socket.interval);
+  const Nsp = io.of(/^\/dynamic-\d+$/).on('connection', (socket) => {
+    const newNamespace = socket.nsp;
+    if (!onlineMap[socket.nsp.name]) {
+      onlineMap[socket.nsp.name] = {};
+    }
+    socket.on('joinGroup', (data) => {
+      onlineMap[socket.nsp.name][socket.id] = data.gg;
+      newNamespace.emit(
+        'onlineList',
+        Object.values(onlineMap[socket.nsp.name]),
+      );
     });
-
     socket.on('error', (error) => {
       console.error(error);
     });
 
+    socket.on('disconnect', () => {
+      delete onlineMap[socket.nsp.name][socket.id];
+      newNamespace.emit(
+        'onlineList',
+        Object.values(onlineMap[socket.nsp.name]),
+      );
+    });
     socket.on('joinRoom', (data) => {
       socket.join(data.roomId);
       console.log('조인확인', data);
     });
-
     socket.on('leaveRoom', (roomId) => {
       socket.leave(roomId);
     });
-
     socket.on('message', (data) => {
       const { message, roomId, groupUserId, createdAt } = data;
       console.log('채팅 확인', data);
-      //   const date = new Date()
       const msg = { message, groupUserId, createdAt };
-      io.to(roomId).emit('message', msg);
+      newNamespace.to(roomId).emit('message', msg);
     });
   });
 };
+
+// io.on('connection', (socket) => {
+//     const req = socket.request;
+//     console.log('연결완료');
+
+//     socket.on('disconnect', () => {
+//       clearInterval(socket.interval);
+//     });
+
+//     socket.on('error', (error) => {
+//       console.error(error);
+//     });
+
+//     let newRoom = io.of('');
+//     socket.on('joinRoom', (data) => {
+//       socket.join(data.roomId);
+//       console.log('조인확인', data);
+//     });
+
+//     socket.on('leaveRoom', (roomId) => {
+//       socket.leave(roomId);
+//     });
+
+//     socket.on('message', (data) => {
+//       const { message, roomId, groupUserId, createdAt } = data;
+//       console.log('채팅 확인', data);
+//       const msg = { message, groupUserId, createdAt };
+//       io.to(roomId).emit('message', msg);
+//     });
+//   });
+// };
