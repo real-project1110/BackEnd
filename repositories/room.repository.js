5b96747a8738f9dface81f9e6d94c22eda9e38f2
jeonghyeon <kjhhy1005@gmail.com>
2022-12-03 05:45:53 +0900

    fix : 에러수정

--- repositories/room.repository.js
+++ repositories/room.repository.js
@@ -12,8 +12,8 @@ class RoomRepository extends Room {
     const findRoomId = await Room.findOrCreate({
       where: { [Op.and]: [{ groupId }, { users }] },
       dafaults: {
-        groupId: groupId,
-        users: users,
+        groupId: 'groupId',
+        users: 'users',
       },
       raw: true,
     });
