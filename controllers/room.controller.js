b0787eeee35cc93296d9e6dcb2c4a216aef667a6
jeonghyeon <kjhhy1005@gmail.com>
2022-12-08 21:51:29 +0900

    f

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -72,7 +72,7 @@ class RoomController {
       console.log(
         '현재시간이 되어야 합니다.:::::::::::::::::::::::::',
         moment(+timestamps)
-          .add(9, 'hour')
+          .subtract(9, 'hour')
           .format('YYYY-MM-DD HH:mm:ss'),
       );
       const unreadChat = await this.roomService.unreadChat({
