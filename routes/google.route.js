62ec95d503e7bac9fbdc76af2784e006c767742f
hyunjin9603 <hyunjin9603@gmail.com>
2022-12-10 22:32:20 +0900

    머지를 위한 commit

--- routes/google.route.js
+++ routes/google.route.js
@@ -48,89 +48,91 @@
 
 // module.exports = router;
 
-const express = require('exprsss');
+const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { User } = require('../models');
 
-const google = async(req,res,next)=>{
-    try{
-        let {authorization} = req.headers;
-        
-        const googleAccessToken = authorization;
+const google = async (req, res, next) => {
+  try {
+    let { authorization } = req.headers;
 
-        const {data: googleUser} = await axios.get(
-            'https://www.googleapis.com/auth/plus.me',
-            {
-                header:{
-                    Authorization: `Bearer ${googleAccessToken}`,
-                }
-            }
-        );
-        let existingMember = null;
-        existingMember = await User.findOne({
-          where: {
-            snsId: googleUser.id,
-            provider: 'google',
-          },
-        });
-        console.log('111111',existingMember)
-        console.log('22222구글유저 정보내놔',googleUser)
-        if (existingMember === null) {
-          const newMember = await User.create({
-            snsId: googleUser.id,
-            nickname: googleUser.properties.nickname,
-            avatarImg: googleUser.properties.profile_image,
-            email: googleUser.kakao_account.email || null,
-            provider: 'google',
-          });
-    
-          // const accessToken = await generateToken(newMember);
-          const accessToken = jwt.sign(
-            {
-              userId: newMember.userId,
-              email: newMember.email,
-              nickname: newMember.nickname,
-              currentPage: newMember.currentPage,
-              avatarImg: newMember.avatarImg,
-            },
-            process.env.SECRET_KEY,
-            { expiresIn: '7d' },
-          );
-          res.json({
-            success: true,
-            accessToken,
-            nickname: newMember.nickname,
-          });
-        } else {
-          // const accessToken = await generateToken(existingMember);
-          const accessToken = jwt.sign(
-            {
-              userId: existingMember.userId,
-              email: existingMember.email,
-              nickname: existingMember.nickname,
-              currentPage: existingMember.currentPage,
-              avatarImg: existingMember.avatarImg,
-            },
-            process.env.SECRET_KEY,
-            { expiresIn: '7d' },
-          );
-          res.json({
-            success: true,
-            accessToken,
-            currentPage: existingMember.currentPage,
-          });
-        }
-      } catch (error) {
-        next(error);
-      }
-    };
-    
-    // 로그인페이지로 이동
-    router.post('/auth/google', google);
-    // 카카오에서 설정한 redicrect url을 통해 요청 재전달
-    router.get('/auth/kakao/callback');
-    
-    module.exports = router;
+    const googleAccessToken = authorization;
+    console.log('123123123123132', googleAccessToken);
+    const userinfo = await axios.get(
+      'https://www.googleapis.com/oauth2/v2/userinfo?access_token=' + googleAccessToken,
+      {
+        headers: {
+          Authorization: `Bearer ${googleAccessToken}`,
+        },
+      },
+    );
+    console.log('22222구글유저 정보내놔', userinfo);
+    let existingMember = null;
+    existingMember = await User.findOne({
+      where: {
+        snsId: googleUser.googleId,
+        provider: 'google',
+      },
+    });
+    console.log('111111', existingMember);
+
+    if (existingMember === null) {
+      const newMember = await User.create({
+        snsId: googleUser.googleId,
+        nickname: googleUser.profileObj.name,
+        avatarImg: googleUser.profileObj.imageUrl,
+        email: googleUser.profileObj.email || null,
+        provider: 'google',
+      });
+
+      // const accessToken = await generateToken(newMember);
+      const accessToken = jwt.sign(
+        {
+          userId: newMember.userId,
+          email: newMember.email,
+          nickname: newMember.nickname,
+          currentPage: newMember.currentPage,
+          avatarImg: newMember.avatarImg,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+      res.json({
+        success: true,
+        accessToken,
+        nickname: newMember.nickname,
+      });
+    } else {
+      // const accessToken = await generateToken(existingMember);
+      const accessToken = jwt.sign(
+        {
+          userId: existingMember.userId,
+          email: existingMember.email,
+          nickname: existingMember.nickname,
+          currentPage: existingMember.currentPage,
+          avatarImg: existingMember.avatarImg,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+      res.json({
+        success: true,
+        accessToken,
+        currentPage: existingMember.currentPage,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 로그인페이지로 이동
+router.post('/auth/google', google);
+// 카카오에서 설정한 redicrect url을 통해 요청 재전달
+router.get('/auth/google/callback');
+
+module.exports = router;
+
