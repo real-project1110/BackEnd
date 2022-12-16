90805a7203153b80f478a67d036bb2cbd038bbcf
jeonghyeon <kjhhy1005@gmail.com>
2022-12-17 05:55:41 +0900

    test

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const server = require('./app');
+const app = express();
+const fs = require('fs');
+const HTTPS = require('https');
 const port = 4010;
 let service;
 const option = {
@@ -6,6 +9,7 @@ const option = {
   key: fs.readFileSync(process.env.KEY_URL),
   cert: fs.readFileSync(process.env.CERT_URL),
 };
+server = HTTPS.createServer(option, app);
 server.listen(port, () => {
   console.log('HTTPS 서버가 실행되었습니다. 포트 :: ' + port);
 });
