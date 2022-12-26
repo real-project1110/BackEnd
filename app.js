a5569f4e83e122b179f28ccc271679e59da7bb75
jeonghyeon <kjhhy1005@gmail.com>
2022-12-26 12:49:24 +0900

    fix :. Nginx가 들어오는 연결을 처리하고 이를 Node.js 애플리케이션으로 전달하기 때문에 app.js 파일에서 이 작업을 수행할 필요 x

--- app.js
+++ app.js
@@ -1,4 +1,88 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// const port = 4000;
+// const moment = require('moment');
+// require('moment-timezone');
+// const helmet = require('helmet');
+// moment.tz.setDefault('Asia/Seoul');
+// const expressSanitizer = require('express-sanitizer');
+// const cookieParser = require('cookie-parser');
+// const morgan = require('morgan');
+// const http = require('http');
+// const fs = require('fs');
+// const HTTPS = require('https');
+// const swaggerFile = require('./swagger-output.json');
+// const swaggerUi = require('swagger-ui-express');
+// const socket = require('./socket');
+
+// // //*fs and https 모듈 가져오기
+// // const https = require('https');
+// // const fs = require('fs');
+
+// const cors = require('cors');
+// const {
+//   errorLogger,
+//   errorHandler,
+// } = require('./middlewares/error-handler.middleware');
+// const routes = require('./routes');
+// const session = require('cookie-session');
+// // const passport = require('passport');
+// // const passportConfig = require('./passport');
+// // passportConfig();
+// const morganMiddleware = require('./middlewares/morganMiddleware');
+
+// // app.use(
+// //   session({
+// //     resave: false,
+// //     saveUninitialized: false,
+// //     secret: [
+// //       process.env.KAKAO_SECRET,
+// //       process.env.GOOGLE_SECRET,
+// //       process.env.NAVER_SECRET,
+// //     ],
+// //     cookie: {
+// //       httpOnly: true,
+// //       secure: false,
+// //     },
+// //   }),
+// // );
+// // app.use(passport.initialize());
+// // app.use(passport.session());
+// app.use(cors());
+// app.use(helmet());
+// //*morgan 따로 사용시 주석 풀기 (tiny,common,combined,dev) 네종류  // morgan/winston 미들웨어 사용시 주석
+// // app.use(morgan('dev'));
+// //*morgan 따로 사용시 밑에 주석 // morgan/winston 미들웨어 사용시 주석풀기
+// app.use(morganMiddleware);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(expressSanitizer());
+// app.use(cookieParser());
+// app.use('/', routes);
+// app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+// app.use(errorLogger);
+// app.use(errorHandler);
+
+// let server;
+// try {
+//   const option = {
+//     ca: fs.readFileSync(process.env.CA_URL),
+//     key: fs.readFileSync(process.env.KEY_URL),
+//     cert: fs.readFileSync(process.env.CERT_URL),
+//   };
+//   server = HTTPS.createServer(option, app);
+//   server.listen(port, () => {
+//     console.log('HTTPS 서버가 실행되었습니다. 포트 :: ' + port);
+//     socket(server);
+//   });
+// } catch (error) {
+//   app.listen(port, () => {
+//     console.log('HTTP 서버가 실행되었습니다. 포트 :: ' + port);
+//   });
+// }
+
+// module.exports = server;
 const express = require('express');
 const app = express();
 const port = 4000;
@@ -16,10 +100,6 @@ const swaggerFile = require('./swagger-output.json');
 const swaggerUi = require('swagger-ui-express');
 const socket = require('./socket');
 
-// //*fs and https 모듈 가져오기
-// const https = require('https');
-// const fs = require('fs');
-
 const cors = require('cors');
 const {
   errorLogger,
@@ -27,33 +107,11 @@ const {
 } = require('./middlewares/error-handler.middleware');
 const routes = require('./routes');
 const session = require('cookie-session');
-// const passport = require('passport');
-// const passportConfig = require('./passport');
-// passportConfig();
+
 const morganMiddleware = require('./middlewares/morganMiddleware');
 
-// app.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: false,
-//     secret: [
-//       process.env.KAKAO_SECRET,
-//       process.env.GOOGLE_SECRET,
-//       process.env.NAVER_SECRET,
-//     ],
-//     cookie: {
-//       httpOnly: true,
-//       secure: false,
-//     },
-//   }),
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use(cors());
 app.use(helmet());
-//*morgan 따로 사용시 주석 풀기 (tiny,common,combined,dev) 네종류  // morgan/winston 미들웨어 사용시 주석
-// app.use(morgan('dev'));
-//*morgan 따로 사용시 밑에 주석 // morgan/winston 미들웨어 사용시 주석풀기
 app.use(morganMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -65,21 +123,11 @@ app.use(errorLogger);
 app.use(errorHandler);
 
 let server;
-try {
-  const option = {
-    ca: fs.readFileSync(process.env.CA_URL),
-    key: fs.readFileSync(process.env.KEY_URL),
-    cert: fs.readFileSync(process.env.CERT_URL),
-  };
-  server = HTTPS.createServer(option, app);
-  server.listen(port, () => {
-    console.log('HTTPS 서버가 실행되었습니다. 포트 :: ' + port);
-    socket(server);
-  });
-} catch (error) {
-  app.listen(port, () => {
-    console.log('HTTP 서버가 실행되었습니다. 포트 :: ' + port);
-  });
-}
+
+server = http.createServer(app);
+server.listen(port, () => {
+  console.log('HTTP 서버가 실행되었습니다. 포트 :: ' + port);
+  socket(server);
+});
 
 module.exports = server;
