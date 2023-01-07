1c44f04c728871fc7ea38957b977f1f5a5c6b9bc
jeonghyeon <kjhhy1005@gmail.com>
2023-01-07 21:30:28 +0900

    testtest

---
+++ test/fixtures/posts.fixtures.js
@@ -0,0 +1,32 @@
+exports.createPostInsertSchemaByController = {
+  content: 'archepro85',
+  groupId: '1',
+  userId: '1',
+};
+exports.createPostResulttSchemaByController = {
+  createdAt: '2022-10-16T09:34:00.396Z',
+  updatedAt: '2022-10-16T09:34:00.397Z',
+  content: 'archepro85',
+  groupId: '1',
+  groupUserId: '1',
+  category: 0,
+  likeCount: 0,
+  commentCount: 0,
+};
+
+exports.createPostInsertSchema = {
+  content: 'archepro85',
+  groupId: '1',
+  groupUserId: '1',
+  category: 0,
+};
+exports.createPostResulttSchema = {
+  createdAt: '2022-10-16T09:34:00.396Z',
+  updatedAt: '2022-10-16T09:34:00.397Z',
+  content: 'archepro85',
+  groupId: '1',
+  groupUserId: '1',
+  category: 0,
+  likeCount: 0,
+  commentCount: 0,
+};
